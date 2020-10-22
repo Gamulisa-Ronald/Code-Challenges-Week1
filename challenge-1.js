eae8417b828e9e104466634b1c90de4263faef78
Gamulisa-Ronald <gamulisaronnie@gmail.com>
2020-10-22 14:30:58 +0300

    Challenge1 displaying Personal Information Of Gamulisa Ronald

---
+++ challenge-1.js
@@ -0,0 +1,15 @@
+//Personal details
+//Declaring variables and Hoisting them
+let name, age, areaOfResidence,courseOffered;
+//Inittialize the variables with value
+name = "Gamulisa Ronald";
+age = 19;
+areaOfResidence = "Nsambya";
+courseOffered = "Digital Technology";
+//Output the variables
+console.log(
+    "Name: " + name  + 
+    " Age: " + age  +  
+    " Place Of Rsidence: " + areaOfResidence + 
+    " Course Offered: " + courseOffered
+    )
